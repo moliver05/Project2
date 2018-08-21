93464ffd2f45310230788a25210ee8d20da19571
Mark <mo.mangahas@yahoo.com>
2018-08-21 14:32:10 -0700

    fixed errors and added custom styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,84 +2,72 @@ $(document).ready(function() {
 
   alert("Interested in Programming?");
   alert("No options cuz, I know you wantcha ;)");
-  alert("See the hidden tabs below for");
-  alert("MAGIC!");
+  alert("See the hidden tabs below for more info ^^");
 
-    $(".btn").click(function(event){
+    $(".btn1").click(function(event){
     $(".tabcontent").hide();
     event.preventDefault
 });
 
-    $(".btn#click1").click(function(event) {
+    $(".btn1#click1").click(function(event) {
     $("#java").fadeToggle();
     event.preventDefault
 });
 
-    $(".btn#click2").click(function(event) {
+    $(".btn1#click2").click(function(event) {
     $("#Csp").fadeToggle();
     event.preventDefault
 });
 
-  $(".btn#click3").click(function(event) {
+  $(".btn1#click3").click(function(event) {
   $("#ruby").fadeToggle();
-  });
+
 });
 
-
-//
 // back-end
-// var personInput = $("input#person").val();
-// var reasonInput = $("input#address").val();
-// var dateInput = $("input#email").val();
-// var stedInput = $("input#phone").val();
-//
-//   $(".name").text(nameInput);
-//   $(".address").text(addressInput);
-//   $(".email").text(emailInput);
-//   $(".phone").text(phoneInput);
-//
-// });
-//
-// $("#goback").click(function(event) {
-//   event.preventDefault()
-//   $("#frontpage").show();
-//   $("#backpage").hide();
-//
-// });
-//
-//
-// $("#next").click(function(event) {
-//   event.preventDefault()
-//
-//   $("#frontpage").hide();
-//   $("#backpage").show();
+
+// Survey
+    $(".btn2#result").click(function(event) {
+      // $(".btn#result").click(function(event) {
+        event.preventDefault();
+        var Questionaires = $("input:radio[name=choice]:checked").val();
+        if ("#Question1" === "one") {
+          alert("C# is for You!");
+        }
+        else if ("#Question2" === "two") {
+          alert("you can do Java!");
+        }
+        else if ("#Question3" === "three") {
+          alert("you can do Ruby!");
+        }
+
+    // });
+  });
+});
+//         $(".btn2#nexxt").click(function(event) {
+//           event.preventDefault();
+//           $("#frontpage").hide();
+//           $("#backpage").show();
 //
 // });
+
+
+//Sign up
+
+// $("#infos").submit(function(event) {
+// event.preventDefault();
 //
+//   var personInput = $("input#person").val();
+//   var addressInput = $("input#address").val();
+//   var emailInput = $("input#email").val();
+//   var phoneInput = $("input#phone").val();
 //
-//     // back-end
+//   $("#person").text(personInput);
+//   $("#address").text(addressInput);
+//   $("#email").text(emailInput);
+//   $("#phone").text(phoneInput);
 //
-//   $(document).ready(function() {
-//     $("#infos").submit(function(event)  {
-//     event.preventDefault()
-//
-//     $("#survey").click(function(event) {
-//         event.preventDefault();
-//         var Questionaires = $("input:radio[name=choice]:checked").val();
-//         if (Question1 === "one") {
-//           alert("C# is for You!");
-//         }
-//         else if (Question2 === "two") {
-//           alert("you can do Java!");
-//         }
-//         else if (Question3 === "three") {
-//           alert("you can do Ruby!");
-//         }
-//         else ( Question4 === null) {
-//           alert("Please pick one");
-//         }
-//           alert("Click the link below for an awesome school!");
-//         }
+//   return(result);
 //
 //   });
 // });
