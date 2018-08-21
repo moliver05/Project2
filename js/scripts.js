17e3a46bfae804fa979485d45f6f05133acd4607
Mark <mo.mangahas@yahoo.com>
2018-08-21 10:30:27 -0700

    reviewed and made a new draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,83 @@
 $(document).ready(function() {
 
-  $("button").click(function(){
-    $(".tabcontent").hide();
-  })
-
-  $("button.click1").click(function() {
-      $("#java").fadeToggle();
-
-   });
-
-  $("button.click2").click(function() {
-      $("#Csp").fadeToggle();
-
-   });
-
-
-  $("button.click3").click(function() {
-    $("#ruby").fadeToggle();
-
-  });
+  alert("Interested in Programming?");
+  alert("No options cuz, I know you wantcha ;)");
+  alert("See the hidden tabs below for");
+  alert("MAGIC!");
 });
+
+$("button").click(function(){
+  $(".tabcontent").hide();
+})
+
+$("button.click1").click(function() {
+    $("#java").fadeToggle();
+
+ });
+
+$("button.click2").click(function() {
+    $("#Csp").fadeToggle();
+
+ });
+
+
+$("button.click3").click(function() {
+  $("#ruby").fadeToggle();
+
+});
+// back-end
+// var personInput = $("input#person").val();
+// var reasonInput = $("input#address").val();
+// var dateInput = $("input#email").val();
+// var stedInput = $("input#phone").val();
+//
+//   $(".name").text(nameInput);
+//   $(".address").text(addressInput);
+//   $(".email").text(emailInput);
+//   $(".phone").text(phoneInput);
+//
+// });
+//
+// $("#goback").click(function(event) {
+//   event.preventDefault()
+//   $("#frontpage").show();
+//   $("#backpage").hide();
+//
+// });
+//
+//
+// $("#next").click(function(event) {
+//   event.preventDefault()
+//
+//   $("#frontpage").hide();
+//   $("#backpage").show();
+//
+// });
+//
+//
+//     // back-end
+//
+//   $(document).ready(function() {
+//     $("#infos").submit(function(event)  {
+//     event.preventDefault()
+//
+//     $("#survey").click(function(event) {
+//         event.preventDefault();
+//         var Questionaires = $("input:radio[name=choice]:checked").val();
+//         if (Question1 === "one") {
+//           alert("C# is for You!");
+//         }
+//         else if (Question2 === "two") {
+//           alert("you can do Java!");
+//         }
+//         else if (Question3 === "three") {
+//           alert("you can do Ruby!");
+//         }
+//         else ( Question4 === null) {
+//           alert("Please pick one");
+//         }
+//           alert("Click the link below for an awesome school!");
+//         }
+//
+//   });
+// });
--- readme.md
+++ readme.md
@@ -5,15 +5,27 @@
 #### By _**Mark Mangahas**_
 
 ## Description
+As you know, Epicodus students must decide which track they would like to pursue before they enroll. While all languages have similarities, and switching between languages and platforms is very common in both internships and jobs, applicants should still choose the track they wish to complete with care. Currently, Epicodus offers the following tracks in Portland and Seattle:
 
-_A brief summary of all the lessons for this week_
+Ruby/Rails
+CSS/React
+C#/.NET
+(All tracks must complete Intro and JavaScript.)
+
+For this Independent Project, please first revisit our blog post "What Programming Language Should I Learn?" regarding choosing a track, as well as the course descriptions for each track. You may also research on other sites if you need more information about a track other than your own.
+
+Then, create a Track Suggester webpage (using HTML, CSS, JavaScript and jQuery) that asks future students a series of questions about themselves and provides a track suggestion based on their answers. You can ask questions about the languages themselves, the kinds of environments where these languages are used, or anything else you find relevant.
+
+Include a minimum of 5 survey questions.
+Offer 3 different track suggestions.
+Don't forget regular Git commits and a detailed README. Commit messages should always be on-topic and professional in tone.
 
 ## Setup/Installation Requirements
 
-* _Clone this repository_
-* _Open the index.html file in your browser_
-* _or visit <https://github.com/moliver05/Project2>_
-
+* Clone this repository
+* or visit <https://github.com/moliver05/Project2>
+* Open a simple HTTP server in the top level of the cloned directory.
+* Then open your web browser of choice to localhost:8000
 
 ## Technologies Used
 
@@ -25,6 +37,6 @@ _A brief summary of all the lessons for this week_
 
 ### License
 
-*MIT License*
+Legal Copyright (c) 2018 _Epicodus_
 
-Copyright (c) 2018 **_Epicodus_**
+*This software is licensed under the MIT license.*
